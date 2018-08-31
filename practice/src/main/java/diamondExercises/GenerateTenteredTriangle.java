eb01a157e801d44ee4ff960532ff2c36ef5a7056
AScodersWH <895052253@qq.com>
2018-08-31 14:57:25 +0800

    [FEATURE] finish centerTriggle

---
+++ practice/src/main/java/diamondExercises/GenerateTenteredTriangle.java
@@ -0,0 +1,12 @@
+package diamondExercises;
+
+import untils.AsterisksGenetator;
+
+
+public class GenerateTenteredTriangle{
+    public static void main(String args[]) {
+
+        AsterisksGenetator asterisksGenetator = new AsterisksGenetator();
+        asterisksGenetator.centerTriggleGenerator(3);
+    }
+}
--- practice/src/main/java/untils/AsterisksGenetator.java
+++ practice/src/main/java/untils/AsterisksGenetator.java
@@ -7,6 +7,12 @@ public class AsterisksGenetator {
         }
     }
 
+    public void spaceGenerator(int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.print(" ");
+        }
+    }
+
     public void verticalGenerator(int num) {
         for (int i = 0; i < num; i++) {
             System.out.println("*");
@@ -15,8 +21,18 @@ public class AsterisksGenetator {
 
     public void rightTriggleGenerator(int num) {
         for (int i = 0; i < num; i++) {
-            horizontalGenerator(i+1);
+            horizontalGenerator(i + 1);
             System.out.println();
         }
     }
+
+    public void centerTriggleGenerator(int num) {
+        for (int i = 1; i <= num; i++) {
+            spaceGenerator(num-i);
+            horizontalGenerator(i*2-1);
+            System.out.println();
+        }
+    }
+
+
 }
