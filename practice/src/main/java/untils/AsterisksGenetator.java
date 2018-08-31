e513e6bc04a43d7a9538d4753b745676fbfa4428
AScodersWH <895052253@qq.com>
2018-08-31 14:23:43 +0800

    [FEATURE] finish generate vertical asterisks with number

--- practice/src/main/java/untils/AsterisksGenetator.java
+++ practice/src/main/java/untils/AsterisksGenetator.java
@@ -3,13 +3,12 @@ package untils;
 public class AsterisksGenetator {
     public void horizontalGenerator(int num) {
         for (int i = 0; i < num; i++) {
-            System.out.print("*");
         }
     }
 
-    public void verticalGenerator(int ynum,int xnum) {
-        for (int i = 0; i < ynum; i++) {
-            horizontalGenerator(xnum);
+    public void verticalGenerator(int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.println("*");
         }
     }
 }
